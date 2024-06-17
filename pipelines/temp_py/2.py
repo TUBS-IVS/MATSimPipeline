dd6310011e06bc1d669f6ed7358b95b8fb8b4536
tnoud <65134660+tnoud@users.noreply.github.com>
2024-06-17 16:47:03 +0200

    Refactor build_position_on_segment_info function

--- pipelines/temp_py/2.py
+++ pipelines/temp_py/2.py
@@ -2,5 +2,21 @@
 
 # passing int or string doesnt matter
 
-# nested dict is much faster for most operations
+# nested dict is much faster than df for most operations
+
+def build_position_on_segment_info(n: int) -> list[list[int]]:
+    # The first list contains numbers from 0 to n-2
+    original_list = list(range(n-1))
+    result = []
+
+    # Process lists until no more elements can be taken
+    while original_list:
+        result.append(original_list)
+        # Generate the next list by taking every second element starting from index 1 (second element)
+        original_list = original_list[1::2]
+            
+    return result
+
+# Example
+print(build_position_on_segment_info(9))
 
